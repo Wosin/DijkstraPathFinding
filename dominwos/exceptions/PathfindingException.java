5f917ddfaf174997348037ceae0ee1d368a8565c
Dominik Wosinski <d.wosinski@oberthur.com>
2017-07-04 12:02:16 +0200

    Removed duplicates

--- dominwos/exceptions/PathfindingException.java
+++
@@ -1,7 +0,0 @@
-package org.dominwos.exceptions;
-
-public class PathfindingException extends RuntimeException {
-    public PathfindingException(String message) {
-        super(message);
-    }
-}
